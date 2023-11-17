631ee0990074f755c8044103fb94d16211a5a8dc
phieu2106 <phamvanhieu2106@gmail.com>
2023-11-17 11:21:12 +0700

    project upload

---
+++ src/main/java/com/example/datvexemphim/DatvexemphimApplication.java
@@ -0,0 +1,13 @@
+package com.example.datvexemphim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatvexemphimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatvexemphimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/datvexemphim/DatvexemphimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datvexemphim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatvexemphimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
